22e4743bf9d58ccee4375dcd3e8ddc5592a6fcde
LoftusN13 <loftus.niamh@yahoo.ie>
2021-01-28 17:54:27 +0000

    add reset method to clear form on submit

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,6 +3,7 @@ function sendEmail(contactForm){
         "from_name": contactForm.fullname.value,
         "from_email": contactForm.email.value,
         "game_feedback": contactForm.message.value
-    })
+    });
+    document.getElementById("contactForm").reset();
     return false;
 };
