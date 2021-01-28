02aef1b3635b80f76f6cec0527dbd2e4bab3979f
LoftusN13 <loftus.niamh@yahoo.ie>
2021-01-28 18:41:03 +0000

    edits function to create message when form is submitted

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,7 +3,16 @@ function sendEmail(contactForm){
         "from_name": contactForm.fullname.value,
         "from_email": contactForm.email.value,
         "game_feedback": contactForm.message.value
-    });
+    })
+    .then(
+        function(response) {
+            document.getElementById("submit-msg").innerHTML = "Thank you! Your message has been successfully sent!";
+        },
+        function(error) {
+             document.getElementById("submit-msg").innerHTML = "Uh-oh! Something has gone wrong; message has not been sent!";
+        }
+    )
+
     document.getElementById("contactForm").reset();
     return false;
 };
